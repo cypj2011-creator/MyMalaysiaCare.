bb030e83ac10a8c2a4f94369ac81a716852cf18e
cypj2011-creator <cypj2011@gmail.com>
2026-07-27 19:59:39 +0800

    Update Auth.tsx

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -205,6 +205,9 @@ const Auth = () => {
                 <Button type="submit" className="w-full" disabled={loading}>
                   {loading ? "Creating account..." : t("signUpButton")}
                 </Button>
+                   <p className="text-center text-xs text-muted-foreground pt-2">
+                  Protected And Secured By Supabase
+                </p>
               </form>
             </TabsContent>
           </Tabs>
