7f432fcd85190aff71a618ce59ec0cc60ae04d89
cypj2011-creator <cypj2011@gmail.com>
2026-08-07 18:13:54 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -56,6 +56,7 @@ const App = () => (
         </main>
         <Footer />
         <ChatBot />
+        <GuestUpgradeNotice />
         <GuestSessionCleanup />
       </HashRouter>
     </TooltipProvider>
